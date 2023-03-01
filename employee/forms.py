c64989c468f0630646d6d259802908a2e83cc972
Hitanshi-wappnet <hitanshi.wappnet@gmail.com>
2023-03-01 13:02:54 +0530

    Added CSS and features

--- employee/forms.py
+++ employee/forms.py
@@ -6,3 +6,29 @@ class EmployeeForm(forms.ModelForm):
     class Meta:
         model = Employee
         fields = "__all__"
+        widgets = {
+            'id': forms.TextInput(attrs={
+                'class': "data",
+                'placeholder': 'Enter your id here'
+                }),
+            'name': forms.TextInput(attrs={
+                'class': "data",
+                'placeholder': 'Enter your name here'
+                }),
+            'email': forms.EmailInput(attrs={
+                'class': "data",
+                'placeholder': 'Enter your email here'
+                }),
+            'contact': forms.TextInput(attrs={
+                'class': "data",
+                'placeholder': 'Enter your Mobile Number here'
+                }),
+            'address': forms.TextInput(attrs={
+                'class': "data",
+                'placeholder': 'Enter your Address here'
+                }),
+            'salary': forms.TextInput(attrs={
+                'class': "data",
+                'placeholder': 'Enter your Salary here'
+                })
+        }
